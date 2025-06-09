69fbba0b19f41fd6c25c5be9f504b326ef1b3a89
promiselxg <okeydeede@gmail.com>
2025-06-09 16:21:28 +0100

    made changed to livestream controller

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,8 +1,8 @@
 const host = {
-  url: "http://localhost:8080/api/v1",
-  host_url: "http://localhost:5173",
-  // url: "https://hrc-iota.vercel.app/api/v1",
-  // host_url: "https://www.hrccintl.org",
+  //url: "http://localhost:8080/api/v1",
+  //host_url: "http://localhost:3000",
+  url: "https://hrc-iota.vercel.app/api/v1",
+  host_url: "https://www.hrccintl.org",
 };
 
 export default host;
